eeb3a09af2920e2fa8b9e18b686fd47a98206a5d
sys <youseung5153@gmail.com>
2025-03-12 16:45:12 +0900

    size of arry

---
+++ array/array_size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() 
+{
+    int numArr[10] = { 11, 22, 33, 44, 55, 66, 77, 88, 99, 110};
+
+    printf("%d\n", sizeof(numArr));                 // 40: 4바이트 크기의 요소가 10개이므로 40
+    printf("%d\n", sizeof(numArr) / sizeof(int));   // 10: 배열의 크기를 구할때는 
+                                                    // 전체 공간을 요소의 크기로 나눠줌
+    return 0;
+}

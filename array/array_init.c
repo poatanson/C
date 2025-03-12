4fb66ceae67b30a3050e894d479f274f9b2995ff
sys <youseung5153@gmail.com>
2025-03-12 16:35:28 +0900

    hehehehehehehehehe

---
+++ array/array_init.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() 
+{
+    int numArr[10] = { 0, };    // 배열의 요소를 모두 0으로 초기화 
+
+    printf("%d\n", numArr[0]);
+    printf("%d\n", numArr[0]);
+    printf("%d\n", numArr[0]);
+
+    return 0;
+
+}

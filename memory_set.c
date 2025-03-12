d583883a448b4dd7fb1c853f1aa282c7e5ffe25a
sys <youseung5153@gmail.com>
2025-03-12 16:19:29 +0900

    memory_set.c 작성

---
+++ memory_set.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+    long long *numPtr = malloc(sizeof(long long)); // long long의 크기 8바이트만큼 동적 메모리 할당
+
+    memset(numPtr, 0x27, 8);    // numPtr이 가리키는 메모리를 8바이트만큼 0x27로 설정 
+
+    printf("0x%llx\n", *numPtr);     // 0x2727272727272727: 27이 8개 들어가 있음
+
+    free(numPtr);       // 동적으로 할당한 메모리 해제 
+
+    return 0;
+}
